c11d9d426b94b1df1003a83d8b495b2475080923
Abu Huraira <huraira.dogar14@gmail.com>
2026-05-07 18:16:55 +0500

    Week1 work

--- Week1/calculator.py
+++ Week1/calculator.py
@@ -1,11 +1,43 @@
 
-x = float(input("What's x? "))
-y = float(input("What's y? "))
-
-print(x + y)
-
-#Simple program to add 2 numbers
+# 1. Simple program to add 2 integers
+#------------------------------------------
 # x = int(input("What's x? "))
 # y = int(input("What's y? "))
 
 # print(x + y)
+
+# 2.Round decimal values to nearest integer with round()
+#------------------------------------------
+
+# x = float(input("What's x? "))
+# y = float(input("What's y? "))
+
+# z = round(x + y)
+
+# print (f"{z:,}") #print integers separated by ,
+
+# print(z)
+
+
+# 3.Round decimal values to 2 digts or 2 decimal places
+#round (number, digits)
+#------------------------------------------
+# x = float(input("What's x? "))
+# y = float(input("What's y? "))
+
+# # z = round(x + y,2)
+# # z = round( x /y, 2) 
+
+# z = x /y
+# print(f"{z:.2F}")
+
+
+#4. square of a number
+def main():
+    x = int(input("What's x "))
+    print ("Squared of x is", square(x))
+
+def square(n):
+    return n**2
+
+main()
